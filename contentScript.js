2f2311ce0463c69be7693b9819d15283c82a54d2
Salma Farghaly <SalmaFarghaly131@gmail.com>
2021-07-09 20:01:36 +0200

    HashMapPredictions

--- contentScript.js
+++ contentScript.js
@@ -1,13 +1,20 @@
 let tweetsArray = [];
-// let lastTweets=[];
 let prevURL=""
 let curURL=""
-let time =  setTimeout(modifyDOM,2000);
+let cntTweets=0
+let prevtweetIdx=0
+let predictions=[]
+let elements=[]
+let scrolling =false
+let tweetMp=new Map();
+//let time =  setTimeout(function(){ alert("Hello")},5000);
 
-window.addEventListener("scroll", function() {
-    getURL()
-    modifyDOM();
-  });
+
+
+// window.addEventListener("scroll", function() {
+//     getURL()
+//     modifyDOM();
+//   });
 
 
 
@@ -15,86 +22,125 @@ window.addEventListener("scroll", function() {
 function getURL(){
     
     curURL=window.location.href
-    console.log(curURL)
-    console.log(prevURL)
     if(curURL!=prevURL){
-        console.log("Route Changed !!!!")
+        // console.log("Route Changed !!!!")
         tweetsArray=[]
+        predictions=[]
+        elements=[]
         prevURL=curURL
+        cntTweets=0
+        prevtweetIdx=0
+        // tweetMp=new Map();
+        tweetMp.clear()
     }
 }
 
 function modifyDOM() {
-    console.log("Insideeee Modify DOM")
-    //You can play with your DOM here or check URL against your regex
+    // console.log("Insideeee Modify DOM")
     let arrChanged=false
-    console.log("The array elements:", tweetsArray);
-    console.log(JSON.stringify({
-        tweets:tweetsArray
-    }))
 
     $('[data-testid="tweet"]').each(function(index){
         foundTweet = $(this).text();
+        // console.log("foundddd",foundTweet)
+            let newStr = foundTweet.replace(/^[^·]*/g,"");
 
+            newStr=newStr.substring(1);
+            // console.log("3nd replacing dot",newStr)
 
-        if (!tweetsArray.includes(foundTweet)){
-            tweetsArray.push(foundTweet);
+            newStr2 = newStr.replace(/^[1-9]{1,2}/g,"");
+            // console.log("b3d replacing numbers",newStr2)
+
+            if(newStr!=newStr2){
+                newStr2=newStr2.substring(1);
+            }
+            // console.log("b3d replacing h aw m aw s ",newStr2)
+
+            // console.log("WEEEEEEEEEEEEEEEE",newStr2)
+        if (!tweetsArray.includes(newStr2)&&newStr2!==""&&newStr2!==" This tweet might be toxic,click to uncover "){
+            tweetsArray.push(newStr2);
+            // elements=elements.concat(this)
+            // tweetMp.set(newStr2, -1);
             arrChanged=true
         }
     });
+    // console.log("ELemengtttss",elements)
 
 
+   
     if (arrChanged==true){
 
-        fetch(`http://127.0.0.1:8000/predict`,
-            {
-                method:"POST", 
-                headers: {
-                    "Accept": "*/*",
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    tweets:tweetsArray
+      
+        newTweets=tweetsArray.slice(prevtweetIdx)
+        prevtweetIdx=tweetsArray.length
+        console.log("newTweets",newTweets)
+
+        if(newTweets.length!=0){
+        
+            fetch(`http://127.0.0.1:8000/predict`,
+                {
+                    method:"POST", 
+                    headers: {
+                        "Accept": "*/*",
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        tweets:newTweets
+                    })
+                }).then(response => response.json())
+                .then(data => {
+                    for(let k=0;k<data.FinalPred.length;k++){
+                        tweetMp.set(newTweets[k],data.FinalPred[k]);
+                        console.log("In Dataaa",newTweets[k],data.FinalPred[k])
+                    }
+                    console.log("Mapppp",tweetMp)
+                    // predictions.concat(data.FinalPred)
                 })
-            }).then(response => response.json())
-            .then(data => {
-                console.log(data)
-                console.log(data.FinalPred)
-                predictions=data.FinalPred
-                i=0;
-                $('[data-testid="tweet"]').each(function(index){
-                    foundTweet = $(this).text();
-                
-                    
-                    // if (!tweetsArray.includes(foundTweet)){
-                        // tweetsArray.push(foundTweet);
-                        if(!$(this).hasClass("squished") && predictions[i]==1){
-                            $(this).addClass("squished");
-                            $(this).html(`<button class = "tweet" style =" display: inline-block;
-                                padding: 8px 15px;
-                                font-size: 15px;
-                                cursor: pointer;
-                                text-align: center;
-                                text-decoration: none;
-                                outline: none;
-                                color: #fff;
-                                background-color:#17A8F4;
-                                border: none;
-                                border-radius: 15px;
-                                // box-shadow: 0 5px #fff;
-                                margin-bottom:10px;
-                                margin: 0px;"
-                                data-original-content="${encodeURI(foundTweet)}"> This tweet might be toxic,click to uncover </button>`);
-                        chrome.runtime.sendMessage({message: "listeners"}, function(response) {
-                        });
-                        }
-                    // }
-                        i++;
-                });
-            })
-            .catch(exception=>console.log(exception))
+                .catch(exception=>console.log(exception))
+        }
     }
 
-    return document.body.innerHTML;
+        $('[data-testid="tweet"]').each(function(index){
+            foundTweet = $(this).text();
+            let newStr = foundTweet.replace(/^[^·]*/g,"");
+
+            newStr=newStr.substring(1);
+            // console.log("3nd replacing dot",newStr)
+
+            newStr2 = newStr.replace(/^[1-9]{1,2}/g,"");
+            // console.log("b3d replacing numbers",newStr2)
+
+            if(newStr!=newStr2){
+                newStr2=newStr2.substring(1);
+            }
+            // console.log("curTweet",newStr2,tweetMp.get(newStr2))
+        
+                if(!$(this).hasClass("squished") && tweetMp.get(newStr2)==1){
+                    $(this).addClass("squished");
+                    $(this).html(`<button class = "tweet" style =" display: inline-block;
+                        padding: 8px 15px;
+                        font-size: 15px;
+                        cursor: pointer;
+                        text-align: center;
+                        text-decoration: none;
+                        outline: none;
+                        color: #fff;
+                        background-color:#17A8F4;
+                        border: none;
+                        border-radius: 15px;
+                        // box-shadow: 0 5px #fff;
+                        margin-bottom:10px;
+                        margin: 0px;"
+                        data-original-content="${encodeURI(foundTweet)}"> This tweet might be toxic,click to uncover </button>`);
+                chrome.runtime.sendMessage({message: "listeners"}, function(response) {
+                });
+                }
+        });
+
+    // return document.body.innerHTML;
+}
+function detectChange(){
+    getURL();
+    modifyDOM();
 }
 
+setInterval(detectChange, 2000);
--- injectedScript.js
+++ injectedScript.js
@@ -1,9 +1,11 @@
-console.log("E7na fel injecteddd dlwa2tyyyy")
+
 var els = document.getElementsByClassName("tweet");
 for(var z = 0; z < els.length; z++) {
   els[z].addEventListener('click', function(){
     var c = decodeURI(this.getAttribute("data-original-content"));
-    // this.parentNode.innerHTML = c;
-    this.parentNode.innerHTML = "<span style='color: white;'>" + c + "</span>";
+    if(this.parentNode!=null){
+      this.parentNode.classList.remove("squished")
+      this.parentNode.innerHTML = "<span style='color: white;'>" + c + "</span>";
+      }
   });
 }
