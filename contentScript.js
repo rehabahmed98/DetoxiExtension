ec55d67e85f59de1e1ecad5f0279d03e1689caed
SalmaFarghaly <59613960+SalmaFarghaly@users.noreply.github.com>
2021-08-16 22:16:31 +0200

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -1,20 +1,13 @@
 let tweetsArray = [];
-let prevURL=""
+let prevURL=""         
 let curURL=""
 let cntTweets=0
 let prevtweetIdx=0
 let predictions=[]
 let elements=[]
 let scrolling =false
-let tweetMp=new Map();
-//let time =  setTimeout(function(){ alert("Hello")},5000);
+let tweetMp=new Map();   // This maps the tweet (key) to it's prediction (value).
 
-
-
-// window.addEventListener("scroll", function() {
-//     getURL()
-//     modifyDOM();
-//   });
 
 
 
@@ -22,48 +15,40 @@ let tweetMp=new Map();
 function getURL(){
     
     curURL=window.location.href
+    // if the user visits another page , we should clear those variables
     if(curURL!=prevURL){
-        // console.log("Route Changed !!!!")
         tweetsArray=[]
         predictions=[]
         elements=[]
         prevURL=curURL
         cntTweets=0
         prevtweetIdx=0
-        // tweetMp=new Map();
         tweetMp.clear()
     }
 }
 
 function modifyDOM() {
-    // console.log("Insideeee Modify DOM")
+ 
     let arrChanged=false
 
     $('[data-testid="tweet"]').each(function(index){
         foundTweet = $(this).text();
-        // console.log("foundddd",foundTweet)
+        
+        // preforming some preprocessing for the tweet before sending it to backend
             let newStr = foundTweet.replace(/^[^·]*/g,"");
 
             newStr=newStr.substring(1);
-            // console.log("3nd replacing dot",newStr)
+          
 
             newStr2 = newStr.replace(/^[1-9]{1,2}/g,"");
-            // console.log("b3d replacing numbers",newStr2)
-
             if(newStr!=newStr2){
                 newStr2=newStr2.substring(1);
             }
-            // console.log("b3d replacing h aw m aw s ",newStr2)
-
-            // console.log("WEEEEEEEEEEEEEEEE",newStr2)
         if (!tweetsArray.includes(newStr2)&&newStr2!==""&&newStr2!==" This tweet might be toxic,click to uncover "){
             tweetsArray.push(newStr2);
-            // elements=elements.concat(this)
-            // tweetMp.set(newStr2, -1);
-            arrChanged=true
+            arrChanged=true // new tweets have been detected and we want to get their predictions
         }
     });
-    // console.log("ELemengtttss",elements)
 
 
    
@@ -74,6 +59,7 @@ function modifyDOM() {
         prevtweetIdx=tweetsArray.length
         console.log("newTweets",newTweets)
 
+        // sending new detected tweets to backend
         if(newTweets.length!=0){
         
             fetch(`http://127.0.0.1:8000/predict`,
@@ -90,10 +76,10 @@ function modifyDOM() {
                 .then(data => {
                     for(let k=0;k<data.FinalPred.length;k++){
                         tweetMp.set(newTweets[k],data.FinalPred[k]);
-                        console.log("In Dataaa",newTweets[k],data.FinalPred[k])
+
                     }
-                    console.log("Mapppp",tweetMp)
-                    // predictions.concat(data.FinalPred)
+            
+               
                 })
                 .catch(exception=>console.log(exception))
         }
@@ -104,16 +90,15 @@ function modifyDOM() {
             let newStr = foundTweet.replace(/^[^·]*/g,"");
 
             newStr=newStr.substring(1);
-            // console.log("3nd replacing dot",newStr)
+
 
             newStr2 = newStr.replace(/^[1-9]{1,2}/g,"");
-            // console.log("b3d replacing numbers",newStr2)
+
 
             if(newStr!=newStr2){
                 newStr2=newStr2.substring(1);
             }
-            // console.log("curTweet",newStr2,tweetMp.get(newStr2))
-        
+
                 if(!$(this).hasClass("squished") && tweetMp.get(newStr2)==1){
                     $(this).addClass("squished");
                     $(this).html(`<button class = "tweet" style =" display: inline-block;
@@ -136,11 +121,12 @@ function modifyDOM() {
                 }
         });
 
-    // return document.body.innerHTML;
+
 }
 function detectChange(){
-    getURL();
-    modifyDOM();
+    getURL();    // Check if the route has been changed
+    modifyDOM(); // detect if there is new tweets that appeared in the DOM
 }
+
 
 setInterval(detectChange, 2000);
